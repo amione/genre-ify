aa4851e031a7a1383d9dfc4bc58493d3ecd0b613
Andrew Mione <mione.a@gmail.com>
2017-09-07 15:28:29 -0400

    The script and a readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # genre-ify
 A quick and dirty ruby script to create new Spotify playlists from existing playlists based on music genre.
+
+# Purpose
+I have "eclectic taste" in music, so generally when I make a playlist I just throw anything I like in there and then set it to shuffle. However, sometimes I find myself wishing I could sort my playlists by genre so that I could listen to all the tracks that are "classic rock" or "deep house". Unfortunately, Spotify doesn't include genre info on a track by track basis. So what this script does (only using libraries that come bundled with ruby) is grab all of the track ids from a specified playlist and put them in a database, then it fetches the artists associated with those tracks (artists have genres, tracks don't), then it matches those genres back to the tracks. Finally, it will ask you to select a genre to create a new playlist from. It will grab the tracks that match that genre from your existing playlist and create a new one. It's not a perfect solution. The genres can be crazy specific, so if you choose one of those you might only generate a playlist with a handful of tracks in it.  
+
+### Usage
+
+	From your command line:
+	```
+		ruby genre-ify.rb
+	```
+
+1.	Go here and generate an oauth token that has all the permission scopes: https://developer.spotify.com/web-api/console/post-playlist-tracks/  Copy this token, and enter it when prompted by the script.
+
+2. Enter your spotify username
+
+3. Enter the ID of the playlist you want to break down. You can get the ID by right clicking on a playlist and choosing "copy spotify uri". The ID will look like this: 0Zw7uKAwbM810t2isqVahR
+
+4. Once the script has fetched all the relevant info, it will spit out a list of genres. Enter one and hit return. This part of the script uses an SQL like clause. For example: "select track_id from spotify_playlist_tracks where genres like '%rock%';" This would actually match anything in the genre list that has rock in it. So, soft rock, hard rock, progressive rock, etc. You can tweak the query to make it more restrictive.
+
+5. Enter a name for your new playlist.
+
+***
+Some color commentary on the script. Initially I had created this script so that you would need to enter your own Spotify App ID and Secret Key from which I would generate a token, but in order to get a token with enough permissions to create a playlist the user would need to login to spotify and grant access, which means this script would need to act as a web server and catch the oauth callback redirect etc. which was more complicated than I wanted...plus I'm lazy. This was just easier.
---
+++ genre-ify.rb
@@ -0,0 +1,439 @@
+require 'rubygems'
+require 'sqlite3'
+require 'json'
+require 'base64'
+require 'net/http'
+require 'uri'
+
+puts "Please go here: https://developer.spotify.com/web-api/console/post-playlists/"
+puts "Generate a bearer token using the interface that has all the scopes."
+
+print "Enter Your Generated Token Here: "
+scoped_token = gets.strip
+
+print "Enter Your Spotify Username: "
+spotify_user = gets.strip
+
+print "Enter ID of playlist to analyze: "
+playlist_id = gets.strip
+
+
+db = SQLite3::Database.new("#{playlist_id}_spotify.db")
+
+db.execute( "create table spotify_playlist_tracks (id INTEGER PRIMARY KEY, playlist_id TEXT, track_name TEXT, track_id TEXT, track_url TEXT,  artist_name TEXT, artist_id  Text, artist_url TEXT, genres TEXT);")
+
+db.execute( "create table spotify_playlist_artists (id INTEGER PRIMARY KEY, band_id TEXT, band_name TEXT, band_url TEXT, band_genres TEXT);")
+
+db.execute( "create table spotify_playlist_genres (id INTEGER PRIMARY KEY, playlist_genres TEXT);")
+
+##playlist request
+
+uri = URI.parse("https://api.spotify.com/v1/users/#{spotify_user}/playlists/#{playlist_id}/tracks")
+request = Net::HTTP::Get.new(uri)
+request["Accept"] = "application/json"
+request["Authorization"] = "Bearer #{scoped_token}"
+
+req_options = {
+  use_ssl: uri.scheme == "https",
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  http.request(request)
+end
+
+
+##playlist loop begin
+
+playlist_response = JSON.parse(response.body)
+
+track_total = playlist_response["total"].to_i
+
+iterations = (track_total / 100.0).ceil
+
+counter = 0
+
+fetch_next = "first_loop"
+
+while counter <= iterations do
+
+	puts "Please Be patient. Fetching tracks in groups of 100 from playlist with id: #{playlist_id}..."
+
+	if fetch_next != "first_loop" && fetch_next != nil
+
+		#puts fetch_next
+
+		counter += 1
+
+		uri = URI.parse("#{fetch_next}")
+		request = Net::HTTP::Get.new(uri)
+		request["Accept"] = "application/json"
+		request["Authorization"] = "Bearer #{scoped_token}"
+
+		req_options = {
+		  use_ssl: uri.scheme == "https",
+		}
+
+		response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+		  http.request(request)
+		end
+
+		playlist_response = JSON.parse(response.body)
+
+	elsif fetch_next != "first_loop" && fetch_next == nil
+
+		counter +=1
+
+	end
+
+	item_total = playlist_response["items"].count - 1
+
+	for x in 0..item_total do
+
+		local_track = playlist_response["items"][x]["is_local"]
+
+		if local_track != "true"
+
+			track_name = playlist_response["items"][x]["track"]["name"]
+			track_id = playlist_response["items"][x]["track"]["id"]
+			track_url = playlist_response["items"][x]["track"]["href"]
+			artist_name = playlist_response["items"][x]["track"]["artists"][0]["name"]
+			artist_id = playlist_response["items"][x]["track"]["artists"][0]["id"]
+			artist_url = playlist_response["items"][x]["track"]["artists"][0]["href"]
+
+			if track_name.include? "'"
+				track_name_clean = track_name.gsub("'", "''")
+			else
+				track_name_clean = track_name
+			end
+
+			if artist_name.include? "'"
+				artist_name_clean = artist_name.gsub("'", "''")
+			else
+				artist_name_clean = artist_name
+			end
+
+#			puts "insert into spotify_playlist_tracks (playlist_id,track_name,track_id,track_url,artist_name,artist_id,artist_url) values ('#{playlist_id}','#{track_name_clean}','#{track_id}','#{track_url}','#{artist_name_clean}','#{artist_id}','#{artist_url}');"
+
+			db.execute("insert into spotify_playlist_tracks (playlist_id,track_name,track_id,track_url,artist_name,artist_id,artist_url) values ('#{playlist_id}','#{track_name_clean}','#{track_id}','#{track_url}','#{artist_name_clean}','#{artist_id}','#{artist_url}');")
+
+		end
+
+	end
+
+	fetch_next = playlist_response["next"]
+
+end
+
+puts "Generating artist table and fetching artist genres..."
+
+bands = db.execute("select distinct artist_id from spotify_playlist_tracks")
+
+all_bands = bands.count - 1
+
+band_counter = 0
+
+artist_array = []
+
+url_counter = 0
+urls_to_generate = all_bands / 50
+
+for x in 0..all_bands do
+
+	if bands[x][0] != ""
+
+		if band_counter <= 49
+			#puts "band counter is less than or equal to 49"
+			artist_array << bands[x][0]
+
+			if band_counter == 49
+
+				band_counter = 0
+				url_counter += 1
+
+				#puts url_counter
+				#puts "https://api.spotify.com/v1/artists?ids=#{artist_array * ","}"
+
+				uri = URI.parse("https://api.spotify.com/v1/artists?ids=#{artist_array * ","}")
+				#puts "https://api.spotify.com/v1/artists/#{bands[x][0]}"
+				request = Net::HTTP::Get.new(uri)
+				request["Accept"] = "application/json"
+				request["Authorization"] = "Bearer #{scoped_token}"
+
+				req_options = {
+			  		use_ssl: uri.scheme == "https",
+				}
+
+				response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+			  		http.request(request)
+				end
+
+				artist_response = JSON.parse(response.body)
+
+				artist_total = artist_response["artists"].count - 1
+
+				for x in 0..artist_total do
+
+					band_name = artist_response["artists"][x]["name"]
+					#puts band_name
+
+					band_id = artist_response["artists"][x]["id"]
+					#puts band_id
+
+					band_url = artist_response["artists"][x]["href"]
+					band_genres = artist_response["artists"][x]["genres"]
+
+					puts "artist name: #{band_name} | spotify_id: #{band_id}"
+
+					if band_name.include? "'"
+						band_name_clean = band_name.gsub("'", "''")
+					else
+						band_name_clean = band_name
+					end
+
+					non_array_band_genres = band_genres * ","
+
+					db.execute("insert into spotify_playlist_artists (band_id, band_name, band_url, band_genres) values ('#{band_id}','#{band_name_clean}','#{band_url}','#{non_array_band_genres}');")
+
+				end
+
+				artist_array = []
+
+				sleep 2
+			else
+				band_counter += 1
+
+			end # end band counter == 49
+
+		end #end band counter <= 49
+
+	end #end if bands != ""
+
+end #end all_bands do
+
+url_counter += 1
+#puts url_counter
+#puts "https://api.spotify.com/v1/artists?ids=#{artist_array * ","}"
+
+uri = URI.parse("https://api.spotify.com/v1/artists?ids=#{artist_array * ","}")
+#puts "https://api.spotify.com/v1/artists/#{bands[x][0]}"
+request = Net::HTTP::Get.new(uri)
+request["Accept"] = "application/json"
+request["Authorization"] = "Bearer #{scoped_token}"
+
+req_options = {
+		use_ssl: uri.scheme == "https",
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+		http.request(request)
+end
+
+artist_response = JSON.parse(response.body)
+
+artist_total = artist_response["artists"].count - 1
+
+for x in 0..artist_total do
+
+	band_name = artist_response["artists"][x]["name"]
+#	puts band_name
+
+	band_id = artist_response["artists"][x]["id"]
+#	puts band_id
+
+	band_url = artist_response["artists"][x]["href"]
+	band_genres = artist_response["artists"][x]["genres"]
+
+	puts "artist name: #{band_name} | spotify_id: #{band_id}"
+
+	if band_name.include? "'"
+		band_name_clean = band_name.gsub("'", "''")
+	else
+		band_name_clean = band_name
+	end
+
+	non_array_band_genres = band_genres * ","
+
+	db.execute("insert into spotify_playlist_artists (band_id, band_name, band_url, band_genres) values ('#{band_id}','#{band_name_clean}','#{band_url}','#{non_array_band_genres}');")
+
+end
+
+puts "Building Genre Selection Table..."
+
+big_genre_array = db.execute( "select band_genres from spotify_playlist_artists;")
+
+genre_groups = big_genre_array.count - 1
+
+huge_genre_array = []
+
+for x in 0..genre_groups do
+	huge_genre_array << big_genre_array[x][0]
+end
+
+smoosh_genres = huge_genre_array * ","
+
+smoosh_and_split = smoosh_genres.split(",")
+
+unique_smoosh_and_split = smoosh_and_split.uniq
+
+reduced_genre_count = unique_smoosh_and_split.count - 1
+
+for x in 0..reduced_genre_count do
+	solo_genre = unique_smoosh_and_split[x]
+	if solo_genre != ""
+		db.execute( "insert into spotify_playlist_genres (playlist_genres) values ('#{solo_genre}');")
+	end
+end
+
+puts "Matching genres back to original playlist tracks..."
+
+artist_genre_arrays = db.execute("select band_id, band_genres from spotify_playlist_artists;")
+
+aga_count = artist_genre_arrays.count - 1
+
+for x in 0..aga_count do
+
+	band_id_lookup = artist_genre_arrays[x][0]
+	band_genre_lookup = artist_genre_arrays[x][1]
+
+	if band_genre_lookup != ""
+		db.execute("update spotify_playlist_tracks set genres = '#{band_genre_lookup}' where artist_id = '#{band_id_lookup}';")
+	end
+
+end
+
+puts "Please select a genre from the following list that you want to create a new playlist from based on your existing playlist:"
+
+sleep 5
+
+genre_values = db.execute("select playlist_genres from spotify_playlist_genres;")
+
+gv_count = genre_values.count - 1
+
+for x in 0..gv_count do
+	puts genre_values[x]
+end
+
+print "Enter Your Genre Selection: "
+genre_selection = gets.strip
+
+puts "Selecting tracks that match your genre selection..."
+
+matching_tracks = db.execute("select track_id from spotify_playlist_tracks where genres like '%#{genre_selection}%';")
+
+match_count = matching_tracks.count - 1
+
+#for x in 0..match_count do
+#	puts "https://open.spotify.com/track/#{matching_tracks[x][0]}"
+#end
+
+print "Enter Your New Playlist Name: "
+new_playlist_name = gets.strip
+
+uri = URI.parse("https://api.spotify.com/v1/users/#{spotify_user}/playlists")
+request = Net::HTTP::Post.new(uri)
+request.content_type = "application/json"
+request["Accept"] = "application/json"
+request["Authorization"] = "Bearer #{scoped_token}"
+request.body = JSON.dump({
+  "description" => "#{genre_selection} playlist created programmatically from playlist with ID = #{playlist_id}",
+  "public" => false,
+  "name" => "#{new_playlist_name}"
+})
+
+req_options = {
+  use_ssl: uri.scheme == "https",
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  http.request(request)
+end
+
+puts "#{new_playlist_name} created..."
+#puts response.body
+
+new_playlist_response = JSON.parse(response.body)
+new_playlist_id = new_playlist_response["id"]
+
+puts "Adding selected tracks to #{new_playlist_name}..."
+
+uri_array = []
+
+track_add_counter = 0
+uri_array_counter = 0
+
+uri_arrays_to_generate = match_count / 100
+#puts uri_arrays_to_generate
+
+for x in 0..match_count do
+
+	if matching_tracks[x] != nil
+
+		if track_add_counter <= 99
+			#puts "band counter is less than or equal to 99"
+			uri_array << "spotify:track:#{matching_tracks[x][0]}"
+
+			if track_add_counter == 99
+
+				track_add_counter = 0
+				uri_array_counter += 1
+
+#				puts uri_array_counter
+#				puts uri_array.uniq
+
+				uri = URI.parse("https://api.spotify.com/v1/users/#{spotify_user}/playlists/#{new_playlist_id}/tracks")
+				request = Net::HTTP::Post.new(uri)
+				request.content_type = "application/json"
+				request["Accept"] = "application/json"
+				request["Authorization"] = "Bearer #{scoped_token}"
+				request.body = JSON.dump({
+				  "uris" => uri_array.uniq
+				})
+
+				req_options = {
+				  use_ssl: uri.scheme == "https",
+				}
+
+				response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+				  http.request(request)
+				end
+
+				uri_array = []
+
+				sleep 2
+			else
+				track_add_counter += 1
+
+			end # end track add counter == 99
+
+		end #end track add counter <= 99
+
+	end #end if matching tracks != nil
+
+end #end match count do
+
+uri_array_counter += 1
+#puts uri_array_counter
+
+#puts uri_array.uniq
+
+uri = URI.parse("https://api.spotify.com/v1/users/#{spotify_user}/playlists/#{new_playlist_id}/tracks")
+request = Net::HTTP::Post.new(uri)
+request.content_type = "application/json"
+request["Accept"] = "application/json"
+request["Authorization"] = "Bearer #{scoped_token}"
+request.body = JSON.dump({
+  "uris" => uri_array.uniq
+})
+
+req_options = {
+  use_ssl: uri.scheme == "https",
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  http.request(request)
+end
+
+track_add_response = JSON.parse(response.body)
+
+#puts track_add_response
+puts "Playlist created. Open up Spotify and enjoy!"
